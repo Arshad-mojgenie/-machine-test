20cc6f4016cb2fdf0fca39a8c357d6e1bd96e6b6
Arshad <arshad@mojgenie.com>
2022-06-28 10:42:54 +0530

    routes added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import CharacterDetails from './Components/CharacterDetails'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/character-listing" element={<CharacterListing />} />
+        <Route path="/character-details" element={<CharacterDetails />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
